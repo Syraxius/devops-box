d918055969009b0b8084d02451d6432d67d634c9
Ang Kah Min, Kelvin <syraxius@hotmail.com>
2017-03-05 15:10:14 +0800

    Changed the default repository and the docker version.

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,29 +3,56 @@ set -x
 
 TERRAFORM_VERSION="0.7.13"
 PACKER_VERSION="0.10.2"
-# create new ssh key
+
+# Create new SSH key
 [[ ! -f /home/ubuntu/.ssh/mykey ]] \
 && mkdir -p /home/ubuntu/.ssh \
 && ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
 && chown -R ubuntu:ubuntu /home/ubuntu/.ssh
 
-# install packages
+# Install packages
+sed -i 's/http:\/\/archive.ubuntu.com/http:\/\/mirror.nus.edu.sg/g' /etc/apt/sources.list
 apt-get update
-apt-get -y install docker.io ansible unzip
-# add docker privileges
+
+# Install Ansible and Unzip
+apt-get -y install ansible unzip
+
+# Install Docker
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+	
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+apt-get update
+
+apt-get install docker-ce
+
+# Add docker privileges
 usermod -G docker ubuntu
-# install pip
+
+# Install pip
 pip install -U pip && pip3 install -U pip
 if [[ $? == 127 ]]; then
     wget -q https://bootstrap.pypa.io/get-pip.py
     python get-pip.py
     python3 get-pip.py
 fi
-# install awscli and ebcli
+
+# Install awscli and ebcli
 pip install -U awscli
 pip install -U awsebcli
 
-#terraform
+# Terraform
 T_VERSION=$(terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
 T_RETVAL=${PIPESTATUS[0]}
 
@@ -34,7 +61,7 @@ T_RETVAL=${PIPESTATUS[0]}
 && unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
 && rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
-# packer
+# Packer
 P_VERSION=$(packer -v)
 P_RETVAL=$?
 
@@ -43,5 +70,5 @@ P_RETVAL=$?
 && unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
 && rm packer_${PACKER_VERSION}_linux_amd64.zip
 
-# clean up
+# Clean up
 apt-get clean
